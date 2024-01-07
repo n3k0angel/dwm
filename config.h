3f22ba884bd60944dd1c5620429e8a32341f0812
n3k0angel <nekoangel@incel.email>
2024-01-07 11:17:06 +0200

    brightnessctl patch

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Nimbus Mono:size=10" };
-static const char dmenufont[]       = "Nimbus Mono:size=10";
+static const char *fonts[]          = { "Nimbus Mono:size=14" };
+static const char dmenufont[]       = "Nimbus Mono:size=14";
 static const char col_gray1[]       = "#0e0f14";
 static const char col_gray2[]       = "#532740";
 static const char col_gray3[]       = "#c1c8d1";
@@ -61,8 +61,13 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+static const char *brighter[] = { "brightnessctl", "set", "10%+", NULL };
+static const char *dimmer[]   = { "brightnessctl", "set", "10%-", NULL };
+
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_F6,     spawn,          {.v = dimmer } },
+	{ MODKEY,                       XK_F7,     spawn,          {.v = brighter } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
