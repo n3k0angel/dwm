6a0646614756d94142d5e297a6ce0491e96bb110
n3k0angel <nekoangel@incel.email>
2024-01-20 03:07:34 +0200

    changed font

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Nimbus Mono:size=14" };
-static const char dmenufont[]       = "Nimbus Mono:size=14";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
 static const char col_gray1[]       = "#0e0f14";
 static const char col_gray2[]       = "#532740";
 static const char col_gray3[]       = "#c1c8d1";
